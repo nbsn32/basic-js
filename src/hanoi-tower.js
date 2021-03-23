278016d27e354cc53404bfd465b48d2a46c89282
Andrei Petrushin <andrei.petrushin@gmail.com>
2021-03-23 14:54:55 +0300

    feat: hanoi tower completed

--- src/hanoi-tower.js
+++ src/hanoi-tower.js
@@ -1,6 +1,9 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function calculateHanoi(/* disksNumber, turnsSpeed */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function calculateHanoi(disksNumber, turnsSpeed) {
+  let result = {};
+  //The minimal number of moves required to solve a Tower of Hanoi puzzle is 2^n − 1, where n is the number of disks
+  result.turns = (2 ** disksNumber) - 1;
+  result.seconds = Math.floor(result.turns * (3600 / turnsSpeed));
+  return result;
 };
