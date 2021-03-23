2ac496be3020c73b0f71904daa10ec3e61e75768
Andrei Petrushin <andrei.petrushin@gmail.com>
2021-03-23 13:41:48 +0300

    feat: dream team completed

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,23 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+
+module.exports = function createDreamTeam(members) {
+  if (Array.isArray(members) && members.length == 0) {
+    return null;
+}
+  let mArr = [];
+  if (Array.isArray(members) == "false") { 
+    return false;
+  }
+  else {
+   if (Array.isArray(members)) {
+    members.forEach((item)=>{
+      if (typeof item =='string' ){
+        mArr.push(item.trim().charAt(0).toUpperCase());
+      }
+    });
+   }
+  }
+    return  mArr.sort().join('');
+  
 };
