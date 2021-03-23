71cd85401b77e6965bdba6763b3f67574cb7816a
Andrei Petrushin <andrei.petrushin@gmail.com>
2021-03-23 14:43:49 +0300

    feat: what season completed

--- src/what-season.js
+++ src/what-season.js
@@ -1,6 +1,22 @@
 const CustomError = require("../extensions/custom-error");
+function Season(month) {
+  if (month==11 || month <2) {
+    return 'winter';
+  }
+  else if ( month >= 2 && month <=4) {
+    return 'spring';
+  }
+  else if  (month >= 5 && month <= 7) {
+    return 'summer';
+  }
+  else if (month >= 8 && month <= 10) {
+    return 'fall'
+  }
+}
 
-module.exports = function getSeason(/* date */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function getSeason(date) {
+  if (!date) return 'Unable to determine the time of year!';
+  if (!+date) throw new Error('Not a Date');
+  let month = date.getMonth();
+  return Season(month);
 };
