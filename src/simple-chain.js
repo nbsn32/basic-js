083b73f1fc1f0b39ef6288705061e6c84c78d7eb
Andrei Petrushin <andrei.petrushin@gmail.com>
2021-03-27 00:39:59 +0300

    feat: add simple chain

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -1,25 +1,30 @@
 const CustomError = require("../extensions/custom-error");
 
 const chainMaker = {
+  array: [],
   getLength() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    return this.array.length;
   },
   addLink(value) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    this.array.push(`( ${value} )`);
+      return this;
   },
   removeLink(position) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    if (!this.array[position]) {
+      this.array = [];
+      throw new Error();
+    }
+    this.array.splice(position - 1,1);
+    return this;
   },
   reverseChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    this.array.reverse();
+    return this;
   },
   finishChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    let result  = this.array.join('~~');
+    this.array=[];
+    return  result;
   }
 };
 
