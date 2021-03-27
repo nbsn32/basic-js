8ec1551f261f82a6d36c2e45dc75ab649799cd84
Andrei Petrushin <andrei.petrushin@gmail.com>
2021-03-28 02:17:29 +0300

    feat: extended repeater add

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,31 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
-};
+module.exports = function repeater(str, options) {
+  let repeatTimes = options.repeatTimes || 1,                   //**********************************************************//
+      separator = options.separator || '+',                     //принимаем входное значение ИЛИ(||) используем по умолчанию//
+      additionRepeatTimes = options.additionRepeatTimes || 1,   //                                                          //
+      additionSeparator = options.additionSeparator || '|',    //**********************************************************//
+      arr = [],
+      additionInput;
   
+
+if (options.addition === null) {
+  additionInput = 'null';
+} else if (options.addition == false) {
+  additionInput = 'false';
+} else {
+  additionInput = options.addition || '';
+}
+
+for (let j = 0; j < additionRepeatTimes; j++) {
+  arr.push(`${additionInput}`);
+}
+
+let subStr = arr.join(additionSeparator),
+    res = [];
+
+for (let j = 0; j < repeatTimes; j++) {
+  res.push(`${str}${subStr}`);
+}
+return res.join(separator);
+};
